aa3593049e4089f611a4700dc88348c461bf1a4c
Sonja <son.boschman@gmail.com>
2016-09-08 12:48:55 +0200

    display lijst met klanten

--- src/main/webapp/script/klant.js
+++ src/main/webapp/script/klant.js
@@ -1,6 +1,26 @@
-//$(document).ready(function(){
-//var url = "http://localhost:8080/DeMummiefabriekREST/webresources/klant";
-//
-//
-//
-//}
+$(document).ready(function () {
+
+    var url = "http://localhost:8080/DeMummiefabriekREST/rest/klant";
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: "json",
+        success: function (klanten) {
+            getAlleKlanten(klanten);
+        }
+    });
+    
+    //empty, then repopulate
+    function getAlleKlanten(klanten) {
+        $("#klantgegevens").empty();
+        $.each(klanten, function (ignore, index) {
+            $("#klantgegevens").append("<tr id='" + index.klantId + "'></tr>");
+            $("tr#" + index.klantId).append("<td>" + index.klantId + "</td>");
+            $("tr#" + index.klantId).append("<td>" + index.voornaam + "</td>");
+            $("tr#" + index.klantId).append("<td>" + index.achternaam + "</td>");
+            $("tr#" + index.klantId).append("<td>" + index.emailadres + "</td>");
+        });
+    }
+    
+}); 
