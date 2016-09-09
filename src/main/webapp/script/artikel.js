66d982f6b8ea387fa762724ae59eaa4ffa8b630b
Sonja <son.boschman@gmail.com>
2016-09-09 11:30:28 +0200

    poging tot nieuw artikel

--- src/main/webapp/script/artikel.js
+++ src/main/webapp/script/artikel.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     var url = "http://localhost:8080/DeMummiefabriekREST/rest/artikel";
 
+    //GET, PUT, POST, DELETE
+    
     $.ajax({
         type: 'GET',
         url: url,
@@ -11,7 +13,40 @@ $(document).ready(function () {
         }
     });
     
-    //empty, then repopulate
+    function nieuwArtikel() {
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: url,
+            dataType: "application/json",
+            data: toJSON(),
+            success: function (data, textStatus, jqXHR) {
+                alert("Het artikel is succesvol toegevoegd aan de vooorraad.");
+                getArtikelen();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('Oeps! Er is iets mis gegaan.\n' + errorThrown);
+                getArtikelen();
+            }
+        });
+    }
+    
+        function verwijderArtikel() {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data, textStatus, jqXHR) {
+                alert: ("Het artikel is succesvol verwijderd.")
+                getArtikellijst();
+            },
+            error: function (jqXHR, textStatus, error) {
+                alert: ("Oeps! Er is iets mis gegaan.\n" + error);
+                getArtikellijst();
+            }
+        });
+    }
+    
+    //GET artikelen, populate table
     function getAlleArtikelen(artikelen) {
         $("#artikellijst").empty();
         $.each(artikelen, function (ignoreVar, index) {
@@ -20,11 +55,11 @@ $(document).ready(function () {
             $("tr#" + index.artikelId).append("<td>" + index.artikelnaam + "</td>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelprijs + "</td>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelvoorraad + "</td>");
-            $("tr#" + index.artikelId).append("<td id='verwijder'><button type='button' id='"+ index.artikelId + "'>Verwijderen</button>");
+            $("tr#" + index.artikelId).append("<td class='verwijder'><button type='button' id='" + index.artikelId + "'>Verwijderen</button>");
         });
     }
     
-    function getArtikelen() {
+    function getArtikellijst() {
         $.ajax({
             type: 'GET',
             url: url,
@@ -35,23 +70,59 @@ $(document).ready(function () {
         });
     }
     
-    $(document).on("click", "td#verwijder button", function(event){
+    //validatie
+    $("form#artikelToevoegen").validate({
+        rules: {
+            artikelnaam: {
+                required: true,
+                maxlength: 45
+            },
+            artikelprijs: {
+                required: true,
+                maxlength: 8
+            },
+            artikelvoorraad: {
+                required: true,
+                maxlength: 5
+            }
+        },
+        messages: {
+            artikelnaam: {
+                required: "Een omschrijving van dit artikel is wel handig voor de klant.",
+                maxlength: jQuery.validator.format("Probeer je te beperken tot een artikelnaam van niet meer dan {0} karakters.")
+            },
+            artikelprijs: {
+                required: "Misschien moet je de klant laten weten hoe duur dit product is?",
+                maxlength: jQuery.validator.format("Nou nou, dat is wel wat te duur. Geen bedragen van meer dan {0} cijfers graag.")
+            },
+            artikelvoorraad: {
+                required: "Als we het niet op voorraad hebben, waarom probeer je het dan te verkopen?",
+                maxlength: jQuery.validator.format("Jeetje, zoveel plek hebben we niet hoor. Probeer eens wat minder dan {0} karakters in te kopen.")
+            }
+        }
+    });
+    
+    $("#artikelToevoegen").submit(function (event) {
+        event.preventDefault();
+        if ($("#artikelToevoegen").valid()) {
+            ajaxCreate(url);
+            
+        }
+    });
+    
+    //Button events
+    $(document).on("click", "td.verwijder button", function(event){
+        event.preventDefault();
         var artUrl = url + "/" + event.target.id;
         verwijderArtikel(artUrl);
     });
     
-    function verwijderArtikel() {
-        $.ajax({
-            type: 'DELETE',
-            url: url,
-            success: function (data, textStatus, jqXHR) {
-                getArtikelen();
-                alert: ("Het artikel is succesvol verwijderd.")
-            },
-            error: function (jqXHR, textStatus, error) {
-                alert: ("Oeps! Er is iets mis gegaan.\n" + error);
-            }
+    //Input string to JSON
+    function toJSON() {
+        JSON.stringify({
+            "artikelnaam": $("input#artikelnaam").val(),
+            "artikelprijs": $("input#artikelprijs").val(),
+            "artikelvoorraad": $("input#artikelvoorraad").val()
         });
     }
-    
 }); 
