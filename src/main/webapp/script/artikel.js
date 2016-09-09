3447affe6754178dde28e8b56a802912c163a88e
Sonja <son.boschman@gmail.com>
2016-09-09 09:58:01 +0200

    delete poging 1

--- src/main/webapp/script/artikel.js
+++ src/main/webapp/script/artikel.js
@@ -14,12 +14,43 @@ $(document).ready(function () {
     //empty, then repopulate
     function getAlleArtikelen(artikelen) {
         $("#artikellijst").empty();
-        $.each(artikelen, function (ignore, index) {
+        $.each(artikelen, function (ignoreVar, index) {
             $("#artikellijst").append("<tr id='" + index.artikelId + "'></tr>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelId + "</td>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelnaam + "</td>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelprijs + "</td>");
             $("tr#" + index.artikelId).append("<td>" + index.artikelvoorraad + "</td>");
+            $("tr#" + index.artikelId).append("<td id='verwijder'><button type='knop' id='"+ index.artikelId + "'>Verwijderen</button>");
+        });
+    }
+    
+    function getArtikelen() {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: "json",
+            success: function (artikelen) {
+                getAlleArtikelen(artikelen);
+            }
+        });
+    }
+    
+    $(document).on("click", "td#verwijder knop", function(event){
+        alert: ("Weet u zeker dat u dit artikel wil verwijderen?");
+        var artUrl = url + "/" + event.target.id;
+        verwijderArtikel(artUrl);
+    });
+    
+    function verwijderArtikel() {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (artikelen) {
+                getArtikelen(artikelen);
+            },
+            error: function (error) {
+                alert: ("Oeps! Er is iets mis gegaan.\n" + error);
+            }
         });
     }
     
